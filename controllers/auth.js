7ecc4f9a06e3bb2e58aaaf5afa0cdcc8f335d971
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-07-19 06:39:01 -0400

    login controller finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,32 @@
 const { response } = require("express");
+const User = require("../models/user");
+const bcrypjs = require("bcryptjs");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login ok",
-  });
+const login = async (req, res = response) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user.status) {
+      return res.status(400).json({
+        msg: "user/password is incorrect - email",
+      });
+    }
+    const validPassword = bcrypjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "user/password is incorrect - password",
+      });
+    }
+
+    res.json({
+      msg: "login ok",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Tall to Support",
+    });
+  }
 };
 
 module.exports = {
